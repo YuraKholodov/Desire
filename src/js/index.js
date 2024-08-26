328534e035be4a4a8668156d77ced5db65d30662
Yura Kholodov <yurdast@mail.ru>
2024-08-26 17:00:59 +0300

    Created gallery

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,26 @@
 import { rightside_menu } from "./modules/rightside-menu";
-import { swiper } from "./modules/swiper";
-
 rightside_menu();
+
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+export const swiper = new Swiper(".top__slider", {
+  // Optional parameters
+  loop: true,
+  autoplay: true,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+import mixitup from "mixitup";
+const mixer = mixitup(".gallery__inner", {
+  load: {
+    filter: ".category-living",
+  },
+});
