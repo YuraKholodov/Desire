06e0f12741fa9ac2daa30d8642af407c11863584
Yura Kholodov <yurdast@mail.ru>
2024-08-28 15:13:05 +0300

    28

---
+++ src/js/contacts.js
@@ -0,0 +1,2 @@
+import { rightside_menu } from "./modules/rightside-menu";
+rightside_menu();
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const config = {
-	mode: 'production',
-	entry: {
-		index: './src/js/index.js',
-		about: './src/js/about.js',
-		gallery: './src/js/gallery.js',
-	},
-	output: {
-		filename: '[name].bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  mode: "production",
+  entry: {
+    index: "./src/js/index.js",
+    about: "./src/js/about.js",
+    gallery: "./src/js/gallery.js",
+    contacts: "./src/js/contacts.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
 
 module.exports = config;
