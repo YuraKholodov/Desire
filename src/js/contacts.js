f954770ab8ff7fe9d7206da1683b48a5092381db
Yura Kholodov <yurdast@mail.ru>
2024-08-29 10:33:33 +0300

    Created contacts

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,2 +1,15 @@
 import { rightside_menu } from "./modules/rightside-menu";
 rightside_menu();
+
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+export const swiper = new Swiper(".contacts-slider__swiper", {
+  loop: true,
+  slidesPerView: 10,
+  slidesPerGroup: 10,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
